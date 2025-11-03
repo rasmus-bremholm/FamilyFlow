ff48e84e3a76fb47254eb00d62b0fe1fafcc9454
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-11-03 10:15:21 +0100

    Fix: Changes to Hook format

---
+++ family-flow/lib/useWeekNavigation.js
@@ -0,0 +1,38 @@
+import { useState, useMemo } from "react";
+import dayjs from "dayjs";
+import { getCurrentWeekStart, getNextWeekStart, getPreviousWeekStart, getWeekArray, getWeekNumber, getWeekYear } from "./weekUtils";
+
+export function useWeekNavigation() {
+	const [weekStart, setWeekStart] = useState(getCurrentWeekStart());
+	const currentWeek = useMemo(() => getWeekArray(weekStart), [weekStart]);
+	const weekNumber = useMemo(() => getWeekNumber(weekStart), [weekStart]);
+	const weekYear = useMemo(() => getWeekYear(weekStart), [weekStart]);
+
+	const nextWeek = () => {
+		setWeekStart((prev) => getNextWeekStart(prev));
+	};
+
+	const previousWeek = () => {
+		setWeekStart((prev) => getPreviousWeekStart(prev));
+	};
+
+	const gotToToday = () => {
+		setWeekStart(getCurrentWeekStart());
+	};
+
+	const goToWeek = (weekNum, year = weekYear) => {
+		const newDate = dayjs().isoWeekYear(year).isoWeek(weekNum).startOf("isoWeek");
+		setWeekStart(newDate);
+	};
+
+	return {
+		currentWeek,
+		weekNumber,
+		weekYear,
+		nextWeek,
+		previousWeek,
+		gotToToday,
+		goToWeek,
+		weekStart,
+	};
+}
