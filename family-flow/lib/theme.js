ad900530ce134282930985065b6b8cc94fe79c01
patrikboyton <hello@patrikboyton.com>
2025-11-03 21:49:46 +0100

    Added first tokens to palette in theme

--- family-flow/lib/theme.js
+++ family-flow/lib/theme.js
@@ -3,12 +3,23 @@ import { createTheme } from "@mui/material/styles";
 const theme = createTheme({
 	palette: {
 		primary: {
-			main: "#1976d2",
+			light: "#A7D3B8",
+			main: "#4B9B73",
+			dark: "#3E8F63",
+			contrastText: "#FBFBFB"
 		},
-		secondary: {
-			main: "#dc004e",
+		background: {
+			default: "#F5FAF7",
+			card: "#FBFBFB"
 		},
+		text: {
+			primary: "#2E2E2E",
+			secondary: "#6C9380"
+		}
 	},
+	typography: {
+		fontFamily: ['Roboto', 'Arial', 'sans-serif'].join(','),
+	}
 });
 
 export default theme;
