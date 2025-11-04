deb2c0b47759674016c61b3a10dfe65efa4bb415
Felicia Bengtsson <130352706+Tarawen@users.noreply.github.com>
2025-11-04 10:27:20 +0100

    added basic button to show modal

--- family-flow/app/page.js
+++ family-flow/app/page.js
@@ -5,30 +5,30 @@ import { Container, Button } from "@mui/material";
 import ModalAddPlan from "./components/ModalAddPlan";
 
 export default function Home() {
-  const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(false);
 
-  const handleAddPlan = (data) => {
-    console.log("Plan added:", data);
-    // Här kan du spara i Supabase eller local state
-  };
+    const handleAddPlan = (data) => {
+        console.log("Plan added:", data);
+        // Här kan du spara i Supabase eller local state
+    };
 
-  return (
-    <Container sx={{ mt: 4 }}>
-      {/*       <Button
-        variant="contained"
-        color="success"
-        onClick={() => setOpen(true)}
-        sx={{ borderRadius: 2 }}
-      >
-        + Add Plan
-      </Button>
+    return (
+        <Container sx={{ mt: 4 }}>
+            <Button
+                variant="contained"
+                color="success"
+                onClick={() => setOpen(true)}
+                sx={{ borderRadius: 2 }}
+            >
+                + Add Plan
+            </Button>
 
-      <ModalAddPlan
-        open={open}
-        date="Monday, October 27"
-        onClose={() => setOpen(false)}
-        onSubmit={handleAddPlan}
-      /> */}
-    </Container>
-  );
+            <ModalAddPlan
+                open={open}
+                date="Monday, October 27"
+                onClose={() => setOpen(false)}
+                onSubmit={handleAddPlan}
+            />
+        </Container>
+    );
 }
