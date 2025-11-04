47caf6f91e9cd740a4852784c2a2546a3a92e317
Felicia Bengtsson <130352706+Tarawen@users.noreply.github.com>
2025-11-04 10:51:10 +0100

    updated page.js and add plan button

--- family-flow/app/page.js
+++ family-flow/app/page.js
@@ -1,34 +1,7 @@
 "use client";
 
-import { useState } from "react";
-import { Container, Button } from "@mui/material";
-import ModalAddPlan from "./components/ModalAddPlan";
+import { Container } from "@mui/material";
 
 export default function Home() {
-    const [open, setOpen] = useState(false);
-
-    const handleAddPlan = (data) => {
-        console.log("Plan added:", data);
-        // Här kan du spara i Supabase eller local state
-    };
-
-    return (
-        <Container sx={{ mt: 4 }}>
-            <Button
-                variant="contained"
-                color="success"
-                onClick={() => setOpen(true)}
-                sx={{ borderRadius: 2 }}
-            >
-                + Add Plan
-            </Button>
-
-            <ModalAddPlan
-                open={open}
-                date="Monday, October 27"
-                onClose={() => setOpen(false)}
-                onSubmit={handleAddPlan}
-            />
-        </Container>
-    );
+    return <Container sx={{ mt: 4 }}></Container>;
 }
