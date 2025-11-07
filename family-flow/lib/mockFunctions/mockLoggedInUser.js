e66acd2e8e989d83c65e806822a7cb5473a55d62
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-11-07 12:57:07 +0100

    removed eventID and changed name of familyId

--- family-flow/lib/mockFunctions/mockLoggedInUser.js
+++ family-flow/lib/mockFunctions/mockLoggedInUser.js
@@ -6,8 +6,7 @@ const loggedInUser = {
 	name: "Anna Svensson",
 	email: "anna.svensson@example.com",
 	password: "hashed_password_anna",
-	familyID: "1001",
-	eventsID: ["550e8400-e29b-41d4-a716-446655440000", "550e8400-e29b-41d4-a716-446655440001", "550e8400-e29b-41d4-a716-446655440002"],
+	familyId: "1001",
 };
 
 localStorage.setItem("loggedInUser", JSON.stringify(loggedInUser));
