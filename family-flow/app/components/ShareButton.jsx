b0fbcba513e390556f77b254cb02765f737b42ea
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-11-10 12:09:45 +0100

    Update ShareButton.jsx

--- family-flow/app/components/ShareButton.jsx
+++ family-flow/app/components/ShareButton.jsx
@@ -1,4 +1,4 @@
-import { Box, IconButton, Snackbar, Tooltip } from '@mui/material';
+import { Box, IconButton, Snackbar, Tooltip, Button } from '@mui/material';
 import ShareIcon from '@mui/icons-material/Share';
 import CloseIcon from '@mui/icons-material/Close';
 import { useState } from 'react';
@@ -6,15 +6,32 @@ import { useState } from 'react';
 export default function ShareButton() {
   const [open, setOpen] = useState(false);
 
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
+
   return (
     <>
-      <Box>
+      <Box onClick={handleClick}>
         <Tooltip title="Share" arrow>
           <IconButton>
             <ShareIcon />
           </IconButton>
         </Tooltip>
       </Box>
+      <Snackbar
+        open={open}
+        onClose={handleClose}
+        message="Week Shared"
+        autoHideDuration={2000}
+      />
     </>
   );
 }
