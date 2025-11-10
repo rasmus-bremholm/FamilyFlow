e6435ae0b83502203028be51b9a93ca4e2d3cdb6
Julia Storme <julia.storme@hotmail.com>
2025-11-10 15:11:57 +0100

    ...

--- family-flow/app/components/Notifications.jsx
+++ family-flow/app/components/Notifications.jsx
@@ -2,9 +2,14 @@
 import { SpeedDial, SpeedDialAction, SpeedDialIcon } from '@mui/material';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import Badge from '@mui/material/Badge';
+import { useState } from 'react';
 
 export function Notification() {
-  const notificationsNumber = 1;
+  const [notificationsNumber, setNotificationsNumber] = useState(1);
+
+  const handleNotificationClick = () => {
+    setNotificationsNumber((prev) => (prev > 0 ? prev - 1 : 0));
+  };
 
   return (
     <SpeedDial
@@ -18,6 +23,7 @@ export function Notification() {
             variant="dot"
             color="error"
             invisible={notificationsNumber === 0}
+            overlap="circular"
           >
             <NotificationsIcon />
           </Badge>
@@ -27,6 +33,7 @@ export function Notification() {
             ? 'Notification'
             : 'Du har en tillagd aktivitet'
         }
+        onClick={handleNotificationClick}
       />
     </SpeedDial>
   );
