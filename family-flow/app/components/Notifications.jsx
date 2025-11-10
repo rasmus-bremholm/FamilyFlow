44b2c939da90ec98c57887315930a3c82157ce70
Julia Storme <julia.storme@hotmail.com>
2025-11-10 14:34:33 +0100

    If an activity is added, there is a red dot by the notifications icon (the bell)

--- family-flow/app/components/Notifications.jsx
+++ family-flow/app/components/Notifications.jsx
@@ -1,11 +1,33 @@
+'use client';
 import { SpeedDial, SpeedDialAction, SpeedDialIcon } from '@mui/material';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import Badge from '@mui/material/Badge';
 
 export function Notification() {
+  const notificationsNumber = 0;
+
   return (
     <SpeedDial
       ariaLabel="Notification Speed Dial"
       sx={{ position: 'absolute', top: 16, right: 16 }}
       icon={<SpeedDialIcon />}
-    ></SpeedDial>
+    >
+      <SpeedDialAction
+        icon={
+          <Badge
+            variant="dot"
+            color="error"
+            invisible={notificationsNumber === 0}
+          >
+            <NotificationsIcon />
+          </Badge>
+        }
+        tooltipTitle={
+          notificationsNumber === 0
+            ? 'Notification'
+            : 'Du har en tillagd aktivitet'
+        }
+      />
+    </SpeedDial>
   );
 }
